789515be23f7d72a6b988abfcd341db1652f3c31
pbpyrojust <justin@justwhat.net>
2014-07-14 19:38:30 -0700

    Working on getting update to work

--- app/models/todo.js
+++ app/models/todo.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 module.exports = mongoose.model('Todo', {
 	text : String,
-	votes : String,
+	votes: {type: Number, default: 0},
 	done : Boolean,
 	time : {type: Date, default: Date.now }
 });
